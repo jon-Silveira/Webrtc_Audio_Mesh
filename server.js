2731d434b1e0326b914bb021ef13f620ffbcb633
JonSil94 <jonsilveirades@gmail.com>
2021-11-30 15:47:24 -0500

    New console checker

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const io = require('socket.io')(server)
 app.use('/', express.static('public'))
 
 io.on('connection', (socket) => {
+
+  console.log("connected")
   socket.on('join', (roomId) => {
 
     let numberOfClients = 0;
